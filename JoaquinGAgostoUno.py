79dc94babd863aa4bdf3732be6d4301df585d7a9
LuisSiccardi <50088170+LuisSiccardi@users.noreply.github.com>
2024-08-19 11:36:40 -0300

    Luis
    correcciones

--- JoaquinGAgostoUno.py
+++ JoaquinGAgostoUno.py
@@ -1,27 +1,38 @@
 menu = """
-***************************************
-*1- Introducir notas de matematicas   *
-*2- Salir                             *              
-***************************************
+******************************************
+*1- Introducir notas de matematicas      *
+*2- Mostrar promedio y cantidad de notas *   
+*3- Salir                                *              
+******************************************
 """
+
 notas = []
 contador = 0
+
 while True:
     print(menu)
     opcion = int(input("Ingrese una de las opciones: "))
-    opcion == 1
-    nota = int(input(f"introduce las notas de matematicas: "))
-    
-    if nota > 0 and nota < 11:
-        notas.append(nota)
-        contador = contador + 1
-    for contador in notas():
-        promedio = notas + contador
-    print(f"El promedio de las notas es:{promedio}")
+    if opcion == 1:
+        nota = int(input(f"introduce las notas de matematicas: "))
+        if 0 < nota < 11: #nota > 0 and nota < 11
+            notas.append(nota)
+            contador = contador + 1
+        else:
+            print("No es un valor válido")
+            continue
 
-    opcion == 2
-    print("No has ingresado ninguna nota")
-    print(menu)
+    if opcion == 2:
+        for contador in notas():
+            promedio = (notas + 0)/contador #notas + contador
+            print(f"El promedio de las notas es:{promedio}")
+        print(notas)
+
+    if opcion == 3:
+       print("Nos vemo, chau")
+       break
+
+       # print("No has ingresado ninguna nota")
+       # print(menu)
 
 
 
